41700fe0a59d1e5ff72fd27182cf91489587597e
milenafarf <m.farfulowska@gmail.com>
2014-05-29 00:50:12 +0200

    rejestracja i logowanie dalej nie dziala

--- main/forms.py
+++ main/forms.py
@@ -1,8 +1,19 @@
 from django import forms
-from main.models import User
+from main.models import User, Season, Matchday, Round
+from django.core import validators
 
         
-class UserForm(ModelForm):
+class UserFormSignUp(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['login', 'email', 'password']
+        fields = ('login', 'email', 'password')
+        widgets={
+            'password': forms.PasswordInput()
+        }
+    confirmpassword = forms.CharField(label='Potwierdz haslo', widget=forms.PasswordInput())
+
+
+class UserFormSignIn(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('login', 'email', 'password')
